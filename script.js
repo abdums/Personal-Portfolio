3a410fa0b60bff2c70ac4955ab1ee754e5071e18
abdums <ace272@icloud.com>
2024-04-28 22:48:56 -0400

    Pop ups for project descrptions.

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+//pop up function for task-manager-description
+function showDescription(popupId) {
+  // Find the popup with the specified ID and display it
+  var popup = document.getElementById(popupId);
+  popup.style.display = "flex";
+}
+
+function closePopup(popupId) {
+  // Find the popup with the specified ID and hide it
+  var popup = document.getElementById(popupId);
+  popup.style.display = "none";
+}
